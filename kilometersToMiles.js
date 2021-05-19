04faf3761da28f38388368978da5d86aeefd3d45
Nathan Birch <birchn@byui.edu>
2021-05-19 12:53:53 -0600

    31 start

--- kilometersToMiles.js
+++
@@ -1,6 +0,0 @@
-function calculateMiles() {
-  let kilometers = parseFloat(document.getElementById('kil').value);
-  let milesInKilometers = 0.621371;
-  let miles = kilometers * milesInKilometers;
-  document.getElementById('outputDiv').innerHTML = miles.toFixed(1);
-}
