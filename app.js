ace235c4806b3929f8995dcaaeeaa7a699728a0b
Nathan Birch <birchn@byui.edu>
2021-05-19 11:46:51 -0600

    finished 24

--- app.js
+++
@@ -1,9 +0,0 @@
-function computeAreaOfCircle() {
-  let r = parseFloat(document.getElementById('radius').value);
-
-  let area = Math.PI * r ** 2;
-
-  document.getElementById(
-    'output'
-  ).innerHTML = `The radius: ${r} produces an area of: ${area.toFixed(2)}`;
-}
