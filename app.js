04d4468541721525a0e288ea115dfa39062ed584
Nathan Birch <birchn@byui.edu>
2021-05-19 13:10:01 -0600

    finished 33

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ function getTeams() {
   let studentsPerTeam = Math.floor(students / teams);
 
   let message = teams - remainder + ' teams of ' + studentsPerTeam;
+
+  if (remainder) {
+    message += ' and ' + remainder + ' teams of ' + (studentsPerTeam + 1);
+  }
+
+  document.getElementById('outputDiv').innerHTML = message;
 }
