9f94bd3234f1d6f2040e0ecb79e9e765956d83e6
Nathan Birch <birchn@byui.edu>
2021-05-19 12:39:42 -0600

    29 start

--- cylinder.js
+++
@@ -1,9 +0,0 @@
-function calculateVolume() {
-  let height = parseFloat(document.getElementById('height').value);
-  let radius = parseFloat(document.getElementById('radius').value);
-
-  let volume = Math.PI * radius ** 2 * height;
-
-  document.getElementById('outputDiv').innerHTML =
-    'The volume is: ' + volume.toFixed(2);
-}
