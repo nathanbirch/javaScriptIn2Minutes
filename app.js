9ab22587b5224f29c606d59bdb7e3b5b1f5234c7
Nathan Birch <birchn@byui.edu>
2021-05-19 13:50:19 -0600

    finished 38

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-// https://www.easycalculation.com/engineering/electrical/cable-length-cable-span.php
-// L = S + ((8 * d2) / (3 * S))
-// Where,
-// L = Cable Length
-// S = Cable Span
-// d = Cable Sag
+// https://keisan.casio.com/exec/system/1223388402
+function calculateTorusVolume() {
+  let a = parseFloat(document.getElementById('inner').value);
+  let b = parseFloat(document.getElementById('outer').value);
 
-function computeLengthOfCable() {
-  let span = parseFloat(document.getElementById('span').value);
-  let sag = parseFloat(document.getElementById('sag').value);
+  let volume = (1 / 4) * Math.PI ** 2 * (a + b) * (b - a) ** 2;
 
-  let l = span + (8 * sag ** 2) / (3 * span);
-
-  document.getElementById('outputDiv').innerHTML = Math.ceil(l);
+  document.getElementById('outputDiv').innerHTML = volume;
 }
