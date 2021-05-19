83db05dec12d57eef34a6b13d25c11e5ac6893b2
Nathan Birch <birchn@byui.edu>
2021-05-19 12:59:45 -0600

    32 start

--- app.js
+++
@@ -1,8 +0,0 @@
-function numberOfStacks() {
-  let totalBoxes = parseInt(document.getElementById('totalBoxes').value);
-  let inEachStack = parseInt(document.getElementById('inEachStack').value);
-
-  let numberOfStacks = Math.ceil(totalBoxes / inEachStack);
-
-  document.getElementById('outputDiv').innerHTML = numberOfStacks;
-}
