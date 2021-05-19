a2ea329e72a66e327d1fa4ac14f0c82977c92f0a
Nathan Birch <birchn@byui.edu>
2021-05-19 13:32:35 -0600

    finished 36

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // assume tax removes 15%
+function calculatePay() {
+  let hours = parseFloat(document.getElementById('hours').value);
+  let wage = parseFloat(document.getElementById('wage').value);
+
+  let pay = hours * wage * 0.85;
+
+  document.getElementById('outputDiv').innerHTML = '$' + pay.toLocaleString();
+}
