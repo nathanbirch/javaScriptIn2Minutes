e8641c5c6813325ee3d4092ae4acc934699ed67e
Nathan Birch <birchn@byui.edu>
2021-05-19 10:36:18 -0600

    update

---
+++ 11.js
@@ -0,0 +1,5 @@
+function printMessage() {
+  document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  let someText = document.getElementById('userInput').value;
+  document.getElementById('userTextOutput').innerHTML = someText;
+}
