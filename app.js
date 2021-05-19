c07a278cbba936bf9074f188c2e0f7f4a96f9c4e
Nathan Birch <birchn@byui.edu>
2021-05-19 13:21:17 -0600

    35 start

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-function calculateMileage() {
-  let odometerStart = parseFloat(
-    document.getElementById('odometerStart').value
-  );
-  let odometerEnd = parseFloat(document.getElementById('odometerEnd').value);
-  let gallonsUsed = parseFloat(document.getElementById('gallonsUsed').value);
-
-  let distance = odometerEnd - odometerStart;
-
-  let mileage = distance / gallonsUsed;
-
-  var unnecessaryOutputString =
-    'Congrats! You got ' + Math.round(mileage) + ' miles per gallon!';
-  document.getElementById('outputDiv').innerHTML = unnecessaryOutputString;
-}
+/* age - 220
+65% is min Range
+85% is max Range
+*/
